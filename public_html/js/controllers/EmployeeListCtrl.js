ae1fa03426316f27f6d95093ed552d2475b69174
Jason Ma <mr_haojie_ma@126.com>
2014-03-24 14:30:46 +0800

    删除冗余文件

--- public_html/js/controllers/EmployeeListCtrl.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-controllers.controller('EmployeeListCtrl', ['$scope', 'Employee', function($scope, Employee) {
-        $scope.employees = Employee.query();
-    }]);
--- public_html/js/controllers/ReportListCtrl.js
+++
@@ -1,4 +0,0 @@
-'use strict';
-controllers.controller('ReportListCtrl', ['$scope', '$routeParams', 'Report', function($scope, $routeParams, Report) {
-        $scope.employees = Report.query({employeeId: $routeParams.employeeId});
-    }]);
