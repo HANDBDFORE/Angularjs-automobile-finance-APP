4b4ec3f0f972f63d84df56130a106df662dafeeb
waraiotoko <waraiotoko1108@gmail.com>
2014-03-25 15:10:01 +0800

    dialog test

--- public_html/js/controllers/QuestionnaireController.js
+++ public_html/js/controllers/QuestionnaireController.js
@@ -1,23 +1,27 @@
 'use strict';
-controllers.controller('QuestionnaireController',['$scope',function($scope){
-                $scope.Q1yes = function(){
-                    if(confirm('')){
-                        self.location = '#/calculator'; //跳转到标准信贷
-                    }
-                };
-                $scope.Q1no = function(){
-                    if(confirm('')){
-                        self.location = ''; //跳转到金融方案
-                    }
-                };
-                $scope.Q2yes = function(){
-                    if(confirm('')){
-                        self.location = ''; //跳转到标准信贷
-                    }
-                };
-                $scope.Q2no = function(){
-                    if(confirm('')){
-                        self.location = ''; //跳转到金融方案
-                    }
-                };               
-        }]);
+controllers.controller('QuestionnaireController', ['$scope', function($scope) {
+        $scope.Q1yes = function() {
+            if (confirm('')) {
+                self.location = '#/calculator'; //跳转到标准信贷
+            }
+        };
+        $scope.Q1no = function() {
+            if (confirm('')) {
+                self.location = ''; //跳转到金融方案
+            }
+        };
+        $scope.Q2yes = function() {
+            if (confirm('')) {
+                self.location = ''; //跳转到标准信贷
+            }
+        };
+        $scope.Q2no = function() {
+            if (confirm('')) {
+                self.location = ''; //跳转到金融方案
+            }
+        };
+        $scope.modalShown = false;
+        $scope.toggleModal = function() {
+            $scope.modalShown = !$scope.modalShown;
+        };
+    }]);
